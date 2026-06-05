6b1aac7a0caf76c69bc0ff67426d0ddccf8ed3ca
Davey4936101 <david_daniel@college.harvard.edu>
2026-06-05 16:56:08 -0400

    feat: add Inngest webhook route handler

---
+++ app/api/inngest/route.ts
@@ -0,0 +1,8 @@
+import { serve } from 'inngest/next'
+import { inngest } from '@/inngest/client'
+import { dealScanner } from '@/inngest/deal-scanner'
+
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: [dealScanner],
+})
